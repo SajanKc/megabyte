0aa2f20903100fc8de4032cf56f6d601f9118ecb
Sajan Kc <sajan1996kc@gmail.com>
2020-11-24 13:21:20 +0545

    about us, case study section added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,28 @@
 // Owl Carousel
 $(document).ready(function () {
-  // Client review slider
+  // Case Studies Slider
+  $("#case__slider").owlCarousel({
+    navigation: true,
+    autoplay: true,
+    loop: true,
+    paginationSpeed: 400,
+    autoplayHoverPause: false,
+    singleItem: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      1024: {
+        items: 1,
+      },
+    },
+  });
+
+  // Client Review Slider
   $("#owl").owlCarousel({
     navigation: true,
     center: true,
